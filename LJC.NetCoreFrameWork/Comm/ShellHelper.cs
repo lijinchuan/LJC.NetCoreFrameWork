01b5fa0d6fce308c1e53a187c4c2122f220365a4
lijinchuan <403479851@qq.com>
2020-03-26 21:49:57 +0800

    添加画图类帮助和SHELL类

---
+++ LJC.NetCoreFrameWork/Comm/ShellHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace LJC.NetCoreFrameWork.Comm
+{
+    public static class ShellHelper
+    {
+        public static string Exec(this string cmd)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    StandardOutputEncoding = Encoding.UTF8,
+                    RedirectStandardError = true,
+                    StandardErrorEncoding = Encoding.UTF8,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+
+            string result = process.StandardOutput.ReadToEnd();
+
+            result += process.StandardError.ReadToEnd();
+
+            return result;
+        }
+    }
+}
